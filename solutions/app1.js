96b7cf492c8025aa59031a9fb62dfb578c0565e5
Zolival <zolinak@gmail.com>
2021-11-20 16:12:25 +0100

    solved

--- solutions/app1.js
+++ solutions/app1.js
@@ -1,1 +1,10 @@
+'use strict';
 
+const setCookie = (cookieValue, expirationSec = 15) => {
+    const now = new Date();
+    now.setTime(now.getTime() + (expirationSec * 60 * 1000));
+    const expires = now.toUTCString();
+    document.cookie = `token=${cookieValue}; expires=${expires}; path=/`;
+}
+
+export default setCookie;
--- solutions/app2.js
+++ solutions/app2.js
@@ -1,1 +1,39 @@
+'use strict'
+
+const cookieHandler = {
+    getAll(){
+        const cookies = document.cookie.split(';')
+        .map((item)=> {
+            const [zero, one] = item.split('=');
+            return [zero.trim(), one.trim()]
+        });
+        return Object.fromEntries(cookies);
+    },
+
+    toSessionStorage(){
+        const cookies = this.getAll();
+        for(let key in cookies){
+            sessionStorage.setItem(key, cookies[key])
+        }
+        return sessionStorage
+    },
+    
+    flush(){
+        const cookies = document.cookie.split(`; `)
+        .forEach((item) => {
+            const txt = `${item.split("=")[0]}=; expires=Thu, 01 Jan 1970 00:00:00 GMT`
+            document.cookie = txt
+        })
+        return cookies
+
+    }
+    
+}
+/*
+console.log('getAll = ', cookieHandler.getAll());
+console.log(`toSessionStorage = `, cookieHandler.toSessionStorage());
+console.log(`flush return = `, cookieHandler.flush());
+*/
+
+export { cookieHandler };
 
